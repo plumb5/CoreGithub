13d45c2635e8c2600f7fb4b53e0e5fe0280fd6a4
Plumb5 <Plumb5@DESKTOP-EHJT6IJ>
2024-08-24 19:52:56 +0530

    plumb5

---
+++ Plumb5/Areas/UCP/Controllers/UsersJourneyController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Plumb5.Controllers;
+
+namespace Plumb5.Areas.UCP.Controllers
+{
+    [Area("UCP")]
+    public class UsersJourneyController : BaseController
+    {
+        public UsersJourneyController(IConfiguration _configuration) : base(_configuration)
+        { }
+        public ActionResult Index()
+        {
+            return View("UsersJourney");
+        }
+
+    }
+}
---
+++ Plumb5/wwwroot/js/UsersJourney.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(() => {
+    $('#ui_divContactUCPScreen').css('margin-top', '76px');
+    $('#ui_divCloseUCP').css('display', 'none');
+    $('.sidebar,.barMenu').css('display', 'none');
+    $('.dropdown-item').css('display', 'none');
+    $('a[href="/Login/SignOut"]').css('display', 'block');
+
+    const aid = $.urlParam("aid")
+    const mid = $.urlParam("mid");
+    const cid = $.urlParam("cid")
+    const label = document.getElementById("urlid");
+    if (mid == 0 && cid == 0) { 
+        label.style.display = "inline";
+    }
+    else {
+        label.style.display = "None";
+        ShowContactUCP($.urlParam("mid"), "", $.urlParam("cid"));
+    }
+    
+
+});
